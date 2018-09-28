e801857c2b3d69593292cf0fcd271c8badbec01b
mostwk <moskvichev.95@gmail.com>
2018-09-28 03:28:08 +0300

    added basic angular app

--- django_project/authentication/models.py
+++ django_project/authentication/models.py
@@ -4,58 +4,58 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class AccountManager(BaseUserManager):
-	def create_user(self, email, password=None, **kwargs):
-		if not email:
-			raise ValueError('Users must have a valid email address')
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address')
 
-		if not kwargs.get('username'):
-			raise ValueError('Users must have a valid username')
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username')
 
-		account = self.model(
-			email=self.normalize_email(email),
-			username=kwargs.get('username')
-		)
-		account.is_stuff = False
-		account.set_password(password)
-		account.save()
+        account = self.model(
+            email=self.normalize_email(email),
+            username=kwargs.get('username')
+        )
+        account.is_stuff = False
+        account.set_password(password)
+        account.save()
 
-		return account
+        return account
 
-	def create_superuser(self, email, password, **kwargs):
-		account = self.create_user(email, password, **kwargs)
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
 
-		account.is_stuff = True
-		account.save()
+        account.is_stuff = True
+        account.save()
 
-		return account
+        return account
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
-	email = models.EmailField(unique=True)
-	username = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
 
-	first_name = models.CharField(max_length=40, blank=True)
-	last_name = models.CharField(max_length=40, blank=True)
-	tagline = models.CharField(max_length=40, blank=True)
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=40, blank=True)
 
-	is_staff = models.BooleanField('staff status', default=False)
+    is_staff = models.BooleanField('staff status', default=False)
 
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	objects = AccountManager()
+    objects = AccountManager()
 
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
-	def __unicode__(self):
-		return self.email
+    def __unicode__(self):
+        return self.email
 
-	def get_full_name(self):
-		return '{} {}'.format(self.first_name, self.last_name)
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
 
-	def get_short_name(self):
-		return self.first_name
+    def get_short_name(self):
+        return self.first_name
 
 
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.vk',
     'bootstrap3',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
