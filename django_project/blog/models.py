eb940bb0eba3439c92cd2b18a2eccac7e158da8d
mostwk <moskvichev.95@gmail.com>
2018-11-14 16:42:03 +0300

    fixed datetime format again

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -5,7 +5,7 @@ from django_project.authentication.models import Account
 
 class BlogPost(models.Model):
     author = models.ForeignKey(Account, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now().replace(microsecond=0))
+    date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=120)
     body = models.TextField(default='', max_length=500)
 
