21620f56cbadf5a104bcaf4d70599a0a543946f5
Ubuntu <ubuntu@ip-172-31-47-213.eu-central-1.compute.internal>
2018-11-16 17:51:31 +0000

    basename small fix

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,7 +5,7 @@ from django_project.blog import views
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.BlogPostViewSet)
-router.register(r'posts/(?P<post_id>.+?)/comments', views.PostCommentViewSet, basename='postcomment')
+router.register(r'posts/(?P<post_id>.+?)/comments', views.PostCommentViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
