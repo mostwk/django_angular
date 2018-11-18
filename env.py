7066cdb87f6859f66911e48f6c9daa8aaea1354d
mostwk <moskvichev.95@gmail.com>
2018-11-18 21:08:42 +0300

    added env for testing

---
+++ env.py
@@ -0,0 +1,15 @@
+
+def SECRET_KEY():
+    return "+pp1^tsewrq2rmnug*(its!g4_r@$qau@#18)mna%un@d8^7qz"
+
+
+def DB_PASS():
+    return "andrey1995"
+
+
+def DB_USER():
+    return "mostwk"
+
+
+def DB_NAME():
+    return "django_angular_db"
